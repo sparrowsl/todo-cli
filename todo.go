78434508bd4843d5a73aef47eb17708b6b6ef6be
sparrowsl <benthorpe.dev@gmail.com>
2024-02-15 14:33:44 +0000

    implement save & get methods for list

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -58,7 +61,33 @@ func (l *List) Delete(i int) error {
 }
 
 // Saves the list of items to a file using the JSON format
-func (l List) Save() {}
+func (l *List) Save(filename string) error {
+	// Marshal the current list items into a JSON of list - json array of item
+	jsonMarshal, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	// writes to the filename or creates it if file doesnt exists
+	return os.WriteFile(filename, jsonMarshal, 0644)
+}
 
 // Obtains a list of items from a saved JSON file
-func (l List) Get() {}
+func (l *List) Get(filename string) error {
+	// Try to read file, return error if file not found
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+
+		return err
+	}
+
+	// Do nothing if file exists but is empty
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
+}
