79bb3df70e6146d23b8f9b38ff7dbf4d35f97d0c
sparrowsl <benthorpe.dev@gmail.com>
2024-02-17 08:27:14 +0000

    list all items if no flag is passed

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/sparrowsl/todo-cli"
+)
+
+const todoFile = ".todo.json"
+
 func main() {
+	list := &todo.List{}
+
+	if err := list.Get(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *list {
+			fmt.Println(item.Task)
+		}
+
+	}
 
 }
